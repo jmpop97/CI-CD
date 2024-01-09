d0d88044a2da1f9960a4d00719a42b7a31071796
jmpop <pjm970128@gmail.com>
2024-01-09 21:08:29 +0900

    1

--- test.py
+++ test.py
@@ -4,16 +4,8 @@ N=10**5
 n=10**3
 
 test=[i for i in range(N)]
-record=[None for _ in range(N)]
-while test:
-    start= time.time()
-    i=test[0]
-    record[i]=time.time()-start
-    test.pop()
-record=record[::n]
-record=record[::-1]
 
 # record.append(time.time()-start)
 with open("list_3.pkl","wb") as f:
-    pickle.dump(record,f)
+    pickle.dump(test,f)
 print("hello world")
