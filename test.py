2c952d7fe7bae1e5cdc2ae3b7667c18170c7dd70
jmpop <pjm970128@gmail.com>
2024-01-09 22:37:20 +0900

    test

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import pickle
 import time
+import sys
 N=10**5
 n=10**3
 
-test=input("check");
+test=sys.argv[0];
+test2=sys.argv[1];
 
 # record.append(time.time()-start)
 with open("list_3.pkl","wb") as f:
