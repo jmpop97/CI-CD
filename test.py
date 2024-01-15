82f1e8b2e70042b16b3459b6f98353ec1df38680
jmpop <88295468+jmpop@users.noreply.github.com>
2024-01-15 16:15:53 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,12 @@ N=10**5
 n=10**3
 
 test=sys.argv[1:];
-
+if test[0] == "main":
+    print("main")
+elif test[0] == "sub":
+    print("sub")
+else:
+    raise
 # record.append(time.time()-start)
 with open("list_5.pkl","wb") as f:
     pickle.dump(test,f)
